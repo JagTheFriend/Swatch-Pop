8043a05cae207791e93e2db17dd9ac6b3410b403
JagTheFriend <JagTheFriend12@gmail.com>
2025-08-01 22:35:58 +0530

    Working on imgs
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/app/_components/CatchPhrase.tsx
+++ src/app/_components/CatchPhrase.tsx
@@ -6,28 +6,31 @@ import { cormorantSC } from "~/fonts";
 export default function CatchPhrase() {
 	return (
 		<section
-			className="grid grid-cols-2 justify-items-center gap-4 bg-[url('/bubbles.svg')] bg-cover px-10 pt-28"
+			className="flex flex-col gap-20 bg-[url('/bubbles.svg')] bg-cover px-10 py-28"
 			id="contact%20us"
 		>
-			<div
-				className={cn(
-					cormorantSC.className,
-					"font-semibold",
-					"text-xl",
-					"md:text-5xl",
-					"flex",
-					"flex-col",
-					"h-full",
-					"justify-evenly",
-					"gap-5",
-				)}
-			>
-				<p>Already know what you want? Let's match it to your vibe.</p>
-				<p>Pick your item, we'll bring the aesthetic!</p>
+			<div className="grid w-full grid-cols-2 justify-items-center gap-4">
+				<div
+					className={cn(
+						cormorantSC.className,
+						"font-semibold",
+						"text-xl",
+						"md:text-5xl",
+						"flex",
+						"flex-col",
+						"h-full",
+						"justify-evenly",
+						"gap-5",
+					)}
+				>
+					<p>Already know what you want? Let's match it to your vibe.</p>
+					<p>Pick your item, we'll bring the aesthetic!</p>
+				</div>
+				<div>
+					<Image src="/hand-bag.svg" alt="palette" />
+				</div>
 			</div>
-			<div>
-				<Image src="/hand-bag.svg" alt="palette" />
-			</div>
+			<Image src="/img-row.svg" alt="img-row" />
 		</section>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,8 +16,8 @@ export default function HomePage() {
 				<Hero1 />
 				<Hero2 />
 				<About />
-				<BallsDivider />
 				<Pallets />
+				<BallsDivider />
 				<CatchPhrase />
 				<LastHeader />
 			</main>
